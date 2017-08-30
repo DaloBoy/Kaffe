b7c0a519e003cd8ca36115d119d940eda800e32e
Daniel Pilanat Østergaard <dani5905@edu.easj.dk>
2017-08-30 09:51:55 +0200

    Oprettet Kaffe Class, som andre Kaffe Klasser Arver fra. Samt oprettet Unit Tests for metoder

---
+++ Kaffe/Kaffe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaffe
+{
+    public abstract class Kaffe
+    {
+        public abstract int pris();
+
+        public string styrke()
+        {
+            
+            return "Stærk";
+        }
+    }
+}
---
+++ KaffeTests/KaffeTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kaffe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaffe.Tests
+{
+    [TestClass()]
+    public class KaffeTests
+    {
+        [TestMethod()]
+        public void prisTestKaffe()
+        {
+            //arrange
+            var kaffe = new SortKaffe();            
+            //act
+            int pris = kaffe.pris();
+            //assert
+            Assert.AreEqual(20, pris);
+        }
+
+        [TestMethod()]
+        public void styrkeTestKaffe()
+        {
+            //arrange
+            var kaffe = new SortKaffe();
+            //act
+            string styrke = kaffe.styrke();
+            //assert
+            Assert.AreEqual("Stærk", styrke);
+        }
+    }
+}
